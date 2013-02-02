c036a656cf1b4ad36367287b93fe1442212a30f7
Miles Johnson <mileswjohnson@gmail.com>
2013-02-01 16:51:07 -0800

    Added access model

--- Model/Player.php
+++ Model/Player.php
@@ -15,4 +15,30 @@ class Player extends TournamentAppModel {
 		)
 	);
 
+	/**
+	 * Grab the users player profile. If it doesn't exist, create it!
+	 *
+	 * @param int $user_id
+	 * @param array $contain
+	 * @return array
+	 */
+	public function getPlayerProfile($user_id, array $contain = array('User')) {
+		$profile = $this->find('first', array(
+			'conditions' => array('Player.user_id' => $user_id),
+			'contain' => $contain
+		));
+
+		if (!$profile && $user_id) {
+			$this->create();
+			$this->save(array('user_id' => $user_id), false);
+
+			return $this->find('first', array(
+				'conditions' => array('Player.id' => $this->id),
+				'contain' => $contain
+			));
+		}
+
+		return $profile;
+	}
+
 }
